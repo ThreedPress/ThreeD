569cd4cb03ed2d86c6895bb6174b8f7f81ee7b41
ThreeDdev <bail-cliche.0l@icloud.com>
2021-12-20 13:38:57 -0800

    initial commit; working on frontend; profile indicator; basic feed layout WiP;

---
+++ frontend/src/components/Profile/ProfileIndicator.vue
@@ -0,0 +1,22 @@
+<template>
+  <ion-item>
+    <ion-avatar slot="start">
+      <img
+        src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y"
+      />
+    </ion-avatar>
+    <ion-label class="ion-hide-sm-down"> username </ion-label>
+  </ion-item>
+</template>
+
+<script lang="ts">
+import { IonItem, IonAvatar, IonLabel } from "@ionic/vue";
+
+export default {
+  components: {
+    IonItem,
+    IonAvatar,
+    IonLabel,
+  },
+};
+</script>
---
+++ frontend/src/views/FeedPage.vue
@@ -0,0 +1,76 @@
+<template>
+  <ion-page>
+    <TopBar />
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense"> </ion-header>
+
+      <div id="container">
+        <strong class="capitalize">{{ $route.params.id }}</strong>
+        <p>Test</p>
+        <ion-grid>
+          <ion-row>
+            <ion-col
+              size="4"
+              v-for="item in [1, 2, 3, 4, 5, 6, 7, 8, 9]"
+              :key="item"
+            >
+              <FeedBox />
+            </ion-col>
+          </ion-row>
+        </ion-grid>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonGrid,
+  IonRow,
+  IonCol,
+} from "@ionic/vue";
+import TopBar from "../components/Layout/TopBar.vue";
+import FeedBox from "../components/Feed/FeedBox.vue";
+export default {
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonGrid,
+    IonRow,
+    IonCol,
+    FeedBox,
+    TopBar,
+  },
+};
+</script>
+
+<style scoped>
+#container {
+  text-align: center;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+  color: #8c8c8c;
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
+</style>
