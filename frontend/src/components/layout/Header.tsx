80e53f7a22994a5a32bfde15bbb3cadd5a9f30bd
ThreeDdev <bail-cliche.0l@icloud.com>
2021-12-23 10:30:11 -0800

    formatted header and header items

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,13 +1,15 @@
 import { HeaderItem } from "./HeaderItem"
-import { MdGridView } from "react-icons/md";
+import { MdGridView, MdAccountCircle, MdAddBox } from "react-icons/md";
 
 export const Header = () => {
     return (
-        <header>
-            <div>
-                <HeaderItem title="feed" Icon={MdGridView}/>
-            </div>
+        <header className="flex flex-col sm:flex-row m-5 justify-between items-center">
             <img className="object-contain" src="/logo.svg" alt="Threed Logo" height={100} width={200}></img>
+            <div className="flex flex-grow justify-evenly max-w-md">
+                <HeaderItem title="feed" Icon={MdGridView}/>
+                <HeaderItem title="profile" Icon={MdAccountCircle}/>
+                <HeaderItem title="create" Icon={MdAddBox}/>
+            </div>
         </header>
     )
 }
--- frontend/src/components/layout/HeaderItem.tsx
+++ frontend/src/components/layout/HeaderItem.tsx
@@ -1,6 +1,6 @@
 export const HeaderItem = ({title, Icon}) => {
     return (
-        <div className="flex flex-col items-center cursor-pointer w-12 sm:w-20 hover:text-white group">
+        <div className="flex flex-col transition motion-reduce:transition-none motion-reduce:transform-none ease-in-out delay-150 items-center cursor-pointer w-12 mx-3 mt-3 sm:w-20 hover:text-white hover:-translate-y-1 hover:scale-110 group">
             <Icon className="h-8 w-8 mb-1 " />
             <p className="tracking-widest uppercase">{title}</p>
         </div>
